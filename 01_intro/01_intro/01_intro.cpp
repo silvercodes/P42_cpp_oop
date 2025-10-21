339083bc8b07c0bd491a01666b0f2e7274b6c8f2
Vladimir Serebrianskiy <silver.itstep@gmail.com>
2025-10-21 23:55:25 +0300

    add: arrays

--- 01_intro/01_intro/01_intro.cpp
+++ 01_intro/01_intro/01_intro.cpp
@@ -15,19 +15,112 @@
 
 
 
-int* add(int* a, int* b)
-{
-	int* res = new int{ *a + *b };
+//int* add(int* a, int* b)
+//{
+//	int* res = new int{ *a + *b };
+//
+//	return res;
+//}
+//
+//int main()
+//{
+//	int* a = new int{ 4 };
+//	int* b = new int{ 5 };
+//
+//	std::cout << *(add(a, b));
+//
+//	return 0;
+//}
 
-	return res;
-}
+
+
+//int main()
+//{
+//	int a = 5;
+//	int* ptr = &a;
+//	std::cout << ptr << '\n';
+//	std::cout << &ptr << '\n';
+//
+//	std::cout << *ptr << '\n';
+//
+//
+//	return 0;
+//}
+
+
+
+
+//int main()
+//{
+//	int a = 5;
+//	int* ptr = &a;
+//	int** p = &ptr;
+//	int*** q = &p;
+//}
+
+
+
+//int main()
+//{
+//	int a = 5;
+//	int* b = new int{ 6 };
+//
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	// int* ptr = new int[3];
+//
+//	int** ptr = new int* [3];
+//
+//	*ptr = new int{ 1018 };
+//
+//	int* val = *ptr;
+//
+//	*(ptr + 1) = new int{ 202 };
+//	*(ptr + 2) = new int{ 303 };
+//
+//
+//}
+
+
 
 int main()
 {
-	int* a = new int{ 4 };
-	int* b = new int{ 5 };
+	int rows = 2;
+	int cols = 3;
 
-	std::cout << *(add(a, b));
+	int** arr = new int* [rows];
+	for (int i{ 0 }; i < rows; ++i)
+	{
+		*(arr + i) = new int[cols];
+		// >>> EQUALS <<<
+		// arr[i] = new int[cols];
+	}
 
-	return 0;
+	for (int i{ 0 }; i < rows; ++i)
+	{
+		int* rowsAddr = *(arr + i);
+		for (int j{ 0 }; j < cols; ++j)
+		{
+			*(rowsAddr + j) = 8;
+		}
+	}
+
+	for (int i{ 0 }; i < rows; ++i)
+	{
+		for (int j{ 0 }; j < cols; ++j)
+		{
+			// std::cout << *(*(arr + i) + j) << ' ';
+			// >>> EQUALS <<<
+			std::cout << arr[i][j] << ' ';
+		}
+		std::cout << '\n';
+	}
 }
+
+
+
